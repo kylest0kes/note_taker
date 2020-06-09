d5bb59dbce9871c794685b7f2cf97d6ebac74f34
kylest0kes <kylestokes1551@gmail.com>
2020-06-09 17:55:00 -0400

    minor code refactor

--- Develop/server.js
+++ Develop/server.js
@@ -5,13 +5,14 @@ const PORT = process.env.PORT || 3000;;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/public", express.static('./public/'));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 app.get("/notes", function (req, res) {
-res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // Starts the server to begin listening
