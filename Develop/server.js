dabb380585f6ef5f987b372e478948d118f9acec
kylest0kes <kylestokes1551@gmail.com>
2020-06-10 19:24:11 -0400

    finished functionality for the application

--- Develop/server.js
+++ Develop/server.js
@@ -16,10 +16,10 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function(req, res) {
   fs.readFile(path.join(__dirname, "db/db.json"), "utf8",
   function(err, data) {
-    console.log(data);
+    //console.log(data);
     return res.json(JSON.parse(data));
   })
-})
+});
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -31,14 +31,33 @@ app.post("/api/notes", function(req, res) {
   function(err, data) {
     const noteDB = JSON.parse(data);
     noteDB.push(newNote);
-    //console.log(noteDB);
     fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(noteDB),
     function(err, data) {
       if (err) throw err;
       return res.json(noteDB);
     })
   });
-})
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  const chosenNote = req.params.id;
+  console.log(chosenNote);
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8",
+  function(err, data) {
+    if (err) throw err;
+    const noteDB = JSON.parse(data);
+    for (let i = 0; i < noteDB.length; i++) {
+      if (chosenNote === noteDB[i].id) {
+        noteDB.splice(i, 1);
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(noteDB),
+        function(err, data) {
+          if (err) throw err;
+          return res.json(noteDB);
+        })
+      }
+    }
+  })
+});
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
